fd5efdd29b71acd7cae93fb9ea3cf9a5cb6049a1
sararo16 <sararo16@educastur.es>
2025-10-18 12:00:54 +0200

    comit

--- UNIDAD 4/FormularioDinamico2.php
+++ UNIDAD 4/FormularioDinamico2.php
@@ -5,12 +5,16 @@
 if (isset($_POST['num1'])) {
     // Mostrar resultados
     $cantidad = intval($_POST['cantidad']);
-    for ($i = 1; $i <= $cantidad; $i++) {
+    for ($i = 1; $i <=$cantidad; $i++) {
         if (isset($_POST["num$i"])) {
             $numero = $_POST["num$i"];
             echo "El número $i es: $numero <br>";
         }
     }
+    echo<<< _END
+    <br> <a href="cajas.html">volver</a>
+    _END;
+
 } elseif (isset($_POST['cantidad'])) {
     // Mostrar formulario
     $cantidad = intval($_POST['cantidad']);
@@ -19,13 +23,14 @@ if (isset($_POST['num1'])) {
     <form method="post" action="FormularioDinamico2.php">   
     _END;
 
-    $cantidad = intval($_POST['cantidad']);
     for ($i = 1; $i <= $cantidad; $i++) {
         echo "<label for=\"num$i\">Número $i:</label>";
-        echo "<input id=\"num$i\" name=\"num$i\">";
+        echo "<input id=\"num$i\" name=\"num$i\" required>";
         echo "<br><br>";
     }
-
+    //se manda la cantidad para usarla en la segunda parte
+    echo "<input type='hidden' name='cantidad' value='$cantidad'>";
+    
     echo <<< _END
     <input type="submit" id="button" value="Enviar">
     </form>
